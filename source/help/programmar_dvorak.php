67ae443f4c8ef48298d11e758200f9c50bc925c5
3ubaidUrRe7man <3ubaidUrRe7man@protonmail.com>
2025-02-07 19:03:25 +0500

    added help

---
+++ source/help/programmar_dvorak.php
@@ -0,0 +1,21 @@
+<?php
+    $pagename = 'Tengwar Keyboard Help';
+    $pagetitle = $pagename;
+    require_once('header.php');
+?>
+
+<p>
+    Tengwar
+</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id="osk" data-states='default shift rightalt rightalt-shift'>
+<h3>Keyboard Layout</h3>
+  <img src="help/typing-guide-desktop.png" alt="Programmar Dvorak Keyboard Layout"></br>
+  <h3>Keyboard Layout (Shift)</h3>
+  <img src="help/typing-guide-desktop-shifted.png" alt="Programmar Dvorak (Shift) Keyboard Layout"></br>
+  <h3>Keyboard Layout (Alt)</h3>
+  <img src="help/typing-guide-desktop-alternate.png" alt="Programmar Dvorak (Alt) Keyboard Layout"></br>
+  <h3>Keyboard Layout (Alt+Shift)</h3>
+  <img src="help/typing-guide-desktop-alternate-shifted.png" alt="Programmar Dvorak (Alt+Shift) Keyboard Layout">
+</div>
