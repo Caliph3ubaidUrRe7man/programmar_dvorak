bea75012509ac04ab85113e47b7a4e5051d6ff1f
3ubaidUrRe7man <3ubaidUrRe7man@protonmail.com>
2025-02-07 19:08:46 +0500

    .

--- source/help/programmar_dvorak.php
+++ source/help/programmar_dvorak.php
@@ -11,11 +11,11 @@
 <h2>Desktop Keyboard Layout</h2>
 <div id="osk" data-states='default shift rightalt rightalt-shift'>
 <h3>Keyboard Layout</h3>
-  <img src="help/typing-guide-desktop.png" alt="Programmar Dvorak Keyboard Layout"></br>
+  <img src="typing-guide-desktop.png" alt="Programmar Dvorak Keyboard Layout"></br>
   <h3>Keyboard Layout (Shift)</h3>
-  <img src="help/typing-guide-desktop-shifted.png" alt="Programmar Dvorak (Shift) Keyboard Layout"></br>
+  <img src="typing-guide-desktop-shifted.png" alt="Programmar Dvorak (Shift) Keyboard Layout"></br>
   <h3>Keyboard Layout (Alt)</h3>
-  <img src="help/typing-guide-desktop-alternate.png" alt="Programmar Dvorak (Alt) Keyboard Layout"></br>
+  <img src="typing-guide-desktop-alternate.png" alt="Programmar Dvorak (Alt) Keyboard Layout"></br>
   <h3>Keyboard Layout (Alt+Shift)</h3>
-  <img src="help/typing-guide-desktop-alternate-shifted.png" alt="Programmar Dvorak (Alt+Shift) Keyboard Layout">
+  <img src="typing-guide-desktop-alternate-shifted.png" alt="Programmar Dvorak (Alt+Shift) Keyboard Layout">
 </div>
